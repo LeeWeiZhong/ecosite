1a00a285c0aba732a3762998739803a16df8e28f
LeeWeiZhong <73062938+LeeWeiZhong@users.noreply.github.com>
2022-02-03 19:49:50 +0800

    Update Ecosite project

--- hamburgerJS.js
+++
@@ -1,9 +0,0 @@
-const hamburgerButton = document.querySelector(".hamburger-button");
-const hamburgerMenu = document.querySelector(".hamburger-menu");
-
-hamburgerButton.addEventListener("click", menu);
-
-function menu() {
-    hamburgerButton.classList.toggle("active");
-    hamburgerMenu.classList.toggle("active");
-}
